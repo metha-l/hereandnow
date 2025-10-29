814b4180b8339b5d59f72e39758528ff401c286a
metha-l <handsson0630@gmail.com>
2025-10-29 19:30:38 +0900

    feat: 카카오 맵 컴포넌트 제작 완료

--- src/common/KakaoMap.tsx
+++ src/common/KakaoMap.tsx
@@ -6,7 +6,23 @@ declare global {
   }
 }
 
-const KakaoMap = () => {
+interface KakaoMapProps {
+  latitude: number;
+  longitude: number;
+  level?: number;
+  width?: string;
+  height?: string;
+  showMarker?: boolean;
+}
+
+const KakaoMap = ({
+  latitude,
+  longitude,
+  level = 3,
+  width = '100%',
+  height = '500px',
+  showMarker = true,
+}: KakaoMapProps) => {
   const mapContainer = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
@@ -21,32 +37,32 @@ const KakaoMap = () => {
         window.kakao.maps.load(initMap);
       };
     }
-  }, []);
+  }, [latitude, longitude]);
 
   const initMap = () => {
     if (mapContainer.current) {
       const mapOption = {
-        center: new window.kakao.maps.LatLng(33.450701, 126.570667),
-        level: 3,
+        center: new window.kakao.maps.LatLng(latitude, longitude),
+        level: level,
       };
 
       const map = new window.kakao.maps.Map(mapContainer.current, mapOption);
 
-      const markerPosition = new window.kakao.maps.LatLng(33.450701, 126.570667);
-      const imageSrc = 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png';
-      const imageSize = new window.kakao.maps.Size(24, 35);
-      const markerImage = new window.kakao.maps.MarkerImage(imageSrc, imageSize);
-
-      const marker = new window.kakao.maps.Marker({
-        position: markerPosition,
-        image: markerImage,
-      });
-
-      marker.setMap(map);
+      if (showMarker) {
+        const markerPosition = new window.kakao.maps.LatLng(latitude, longitude);
+        const imageSrc = 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png';
+        const imageSize = new window.kakao.maps.Size(24, 35);
+        const markerImage = new window.kakao.maps.MarkerImage(imageSrc, imageSize);
+        const marker = new window.kakao.maps.Marker({
+          position: markerPosition,
+          image: markerImage,
+        });
+        marker.setMap(map);
+      }
     }
   };
 
-  return <div style={{ width: '100%', height: '500px' }} ref={mapContainer} />;
+  return <div style={{ width, height }} ref={mapContainer} />;
 };
 
 export default KakaoMap;
